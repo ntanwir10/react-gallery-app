f0a155c47c3015199755076ac8b545171e55d15a
Nauman Tanwir <ntanwir10@gmail.com>
2020-09-06 19:29:23 +0530

    initial commit with firebase config

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAt03uoOMNq60ERIdpfFJ65Ke6-9uqPv6Y",
+  authDomain: "react-fakegram.firebaseapp.com",
+  databaseURL: "https://react-fakegram.firebaseio.com",
+  projectId: "react-fakegram",
+  storageBucket: "react-fakegram.appspot.com",
+  messagingSenderId: "347708669442",
+  appId: "1:347708669442:web:1b31535301fbf64334311b",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
